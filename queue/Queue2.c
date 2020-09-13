dc78d7b0dcbf6c4c4cc75b46cb49d52c12c89085
wujiayang <1064216585@qq.com>
2020-09-13 00:24:19 -0700

    队列

--- queue/Queue2.c
+++ queue/Queue2.c
@@ -1,3 +1,7 @@
+#include <stdio.h> 
+#include <stdbool.h> 
+#define ERROR -1000
+
 typedef int ElementType;
 typedef struct Node* PtrToNode;
 struct Node {
@@ -16,7 +20,7 @@ typedef PtrToQNode Queue;
 
 Queue CreateQ(int MaxSize)
 {
-	Queue Q = (Queue)malloc(sizeof(struct QNode));
+	Queue Q = (Queue)malloc(sizeof(struct QNode));// 
 	Q->Front = Q->Rear = NULL;
 	return Q;
 }
@@ -26,6 +30,7 @@ bool IsEmpty(Queue Q)
 	return (Q->Front == NULL);
 }
 
+//插入，队尾插入一个节点 
 bool addQ(Queue Q, ElementType X)
 {
 	PtrToNode s = (PtrToNode)malloc(sizeof(struct Node));
@@ -39,13 +44,14 @@ bool addQ(Queue Q, ElementType X)
 	s->Next = NULL;
 	Q->Rear->Next = s;
 	Q->Rear = s; //将s设置为当前的队尾 
-	return true； 
+	return true;
 } 
-
+//删除，从队头删掉一个节点 ，front向后移，并释放该节点占用的空间 
 ElementType deleteQ(Queue Q)
 {
 	ElementType e;
-	PtrToNode s = (PtrToNode)malloc(sizeof(struct Node));
+	PtrToNode s;
+	//PtrToNode s = (PtrToNode)malloc(sizeof(struct Node));
 	if (IsEmpty(Q))
 	{
 		printf("队为空\n");
@@ -53,17 +59,24 @@ ElementType deleteQ(Queue Q)
 	}
 	else
 	{
-		s = Q->Front;
+		s = Q->Front; //把要删除的队首节点的地址暂存在s 
 		if (Q->Front == Q->Rear)
-			Q->Front = Q->Next = NULL;
+			Q->Front = Q->Rear = NULL;  //删除后将队列置空 
 		else
 		{
-			Q->Front = Q->Front->Next;
+			Q->Front = Q->Front->Next; //队首节点的地址指向下一个节点 
 			e = s->Data; 
 			free(s);
 			return e;
 		}
-		
-		
+	
 	}
 } 
+
+
+/*
+链队
+ 1、结点的创建：data， next指针 
+ 2、队列的创建：front节点， rear节点， size 
+
+*/ 
