e4498842f10fb3beee175fa0d451d889b975a2b1
jayantwu <31072285+jayantwu@users.noreply.github.com>
2020-11-11 23:46:44 +0800

    Update pointer_fun.c

--- test/pointer_fun.c
+++ test/pointer_fun.c
@@ -4,18 +4,14 @@ void fun1(int x);
 void fun2(int x);
 void fun3(int x);
 void funcall1(void(*fun)(int), int x);
-void funcall2(p, int x;);
 typedef void(*)(int) p;
 
 
 int main()
 {
-	funcall1(fun1, 1);
-	funcall1(fun2, 2);
-	funcall1(fun3, 3);
-	funcall1(fun1, 1);
-	funcall1(fun2, 2);
-	funcall1(fun3, 3);
+	funcall(fun1, 1);
+	funcall(fun2, 2);
+	funcall(fun3, 3);
 	
 	return 0;
 }
@@ -43,10 +39,3 @@ void funcall1(void(*fun)(int), int x)
 {
 	fun(x);
 }
-
-
-void funcall2(p fun, int x)
-{
-	fun(x);
-}
-	
